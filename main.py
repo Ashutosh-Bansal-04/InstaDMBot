64910342b183f407b2dd06187a465f59aa7804cc
Ashutosh_Bansal <ashutoshbansal0410@gmail.com>
2025-05-20 10:49:29 +0530

    Solving error

--- main.py
+++ main.py
@@ -55,11 +55,12 @@ def login_instagram(driver, username, password):
 # === Message Sending Logic ===
 def try_message_button(driver):
     try:
-        buttons = driver.find_elements(By.CLASS_NAME, "x1i10hfl")
-        for btn in buttons:
-            if 'Message' in btn.text:
-                btn.click()
-                print("✅ Clicked primary message button (generic text scan).")
+        # buttons = driver.find_elements(By.CLASS_NAME, "x1i10hfl")
+        buttons = driver.find_element(By.XPATH, "//div[@class='x1i10hfl xjqpnuy xa49m3k xqeqjp1 x2hbi6w x972fbf xcfux6l x1qhh985 xm0m39n xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x18d9i69 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x1lku1pv x1a2a7pz x6s0dn4 xjyslct x1lq5wgf xgqcy7u x30kzoy x9jhf4c x1ejq31n xd10rxx x1sy0etr x17r0tee x9f619 x1ypdohk x78zum5 x1f6kntn xwhw2v2 x10w6t97 xl56j7k x17ydfre x1swvt13 x1pi30zi x1n2onr6 x2b8uid xlyipyv x87ps6o x14atkfc xcdnw81 x1i0vuye x1gjpkn9 x5n08af xsz8vos']").click()
+        # for btn in buttons:
+        #     if 'Message' in btn.text:
+        #         btn.click()
+        #         print("✅ Clicked primary message button (generic text scan).")
         print("✅ Clicked primary message button.")
         return True
     except:
@@ -67,10 +68,20 @@ def try_message_button(driver):
 
 def try_3dot_menu(driver):
     try:
-        menu = driver.find_element(By.XPATH, "//button[contains(@aria-label, 'Options')]")
-        menu.click()
-        time.sleep(random.uniform(2, 4))
-        message_btn = driver.find_element(By.XPATH, "//div[text()='Message']")
+        # menu = driver.find_element(By.XPATH, "//button[contains(@aria-label, 'Options')]")
+        # menu.click()
+        # time.sleep(random.uniform(2, 4))
+        # message_btn = driver.find_element(By.XPATH, "//div[text()='Message']")
+        # message_btn.click()
+        # menu = driver.find_elements(By.CLASS_NAME, "x972fbf")
+        menu = driver.find_element(By.XPATH, "//div[@class='x1i10hfl x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x6s0dn4 xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x1ypdohk x78zum5 xl56j7k x1y1aw1k x1sxyh0 xwib8y2 xurb0ha xcdnw81']").click()
+
+        # for opt in menu:
+        #     # if 'Message' in opt.text:
+        #     opt.click()
+        #     print("✅ Clicked menu button (generic text scan).")
+        #     time.sleep(random.uniform(2, 4))
+        message_btn = driver.find_element(By.XPATH, "//button[text()='Send message']")
         message_btn.click()
         return True
     except:
